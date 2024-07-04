bbf20895754ad448305839f37a5e0b1ae098b8c7
Muzammil71 <zamiikhan713@gmail.com>
2024-07-04 12:22:37 +0500

    Data store in backend

---
+++ WebApplication1/Migrations/20240703113642_stddata.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class stddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    FatherName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Age = table.Column<int>(type: "Int", nullable: false),
+                    DOB = table.Column<int>(type: "Int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ WebApplication1/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Students
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("StudentName", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+        [Column("FatherName", TypeName = "varchar(100)")]
+        public string FatherName { get; set; }
+        [Column("Age", TypeName = "Int")]
+        public int Age { get; set; }
+        [Column("DOB", TypeName = "Int")]
+        public int Birthday { get; set; }
+    }
+}
